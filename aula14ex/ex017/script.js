6ab3a036ceabaa5d376c596550ae210260573715
CamillaFontenele <camillafontenele@gmail.com>
2025-05-15 15:55:21 -0300

    exercicio tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,16 @@
-alert('Olá!')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if(num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for (let c = 1; c <= 10; c++) {
+            let item = document.createElement('option');
+            item.text = `${n} x ${c} = ${n * c}`
+            tab.appendChild(item)
+        }
+    }
+    
+}
